e8e1f2ffda1e287108e2039f29cc1535e7d98820
Alexander Berezovsky <fantik217@gmail.com>
2018-12-17 19:21:46 +0700

    FIX: ellipsis длинных названий в последнем блоке

--- demo/src/index.js
+++ demo/src/index.js
@@ -7,7 +7,23 @@ class Demo extends Component {
   render() {
     return <div>
       <h1>react-breadcrumbs Demo</h1>
-      <Example/>
+      <Example
+        wrap={false}
+        breadcrumbs={[{
+          href: "index",
+          title: "Main",
+          id: 1
+        },
+          {
+            href: "catalog",
+            title: "Complaints",
+            id: 2
+          },{
+            href: "catalog-item",
+            title: "ON_SCHFDOPPR_2BM-7731369928-773101001-201710160158236546138_2BM-7718239655-2013010911292072118020000000000_20181107_570d5a12-4ce4-465b-ba80-00b9c921ccadON_SCHFDOPPR_2BM-7731369928-773101001-201710160158236546138_2BM-7718239655-2013010911292072118020000000000_20181107_570d5a12-4ce4-465b-ba80-00b9c921ccad",
+            id: 3
+          }]}
+      />
     </div>
   }
 }
--- src/components/Breadcrumbs.js
+++ src/components/Breadcrumbs.js
@@ -12,12 +12,38 @@ import { BreadcrumbRightArrow } from "../svg";
 import defaultTheme from '../theme/defaultTheme';
 
 class Breadcrumbs extends Component {
+
   constructor(props) {
     super(props);
 
     this.displayName = "Breadcrumbs";
+    this.container = React.createRef();
+
+    this.state = {
+      widthOfLast: null
+    };
 
     this.renderBreadcrumb = this.renderBreadcrumb.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount', this.container);
+    try {
+
+      const c = this.container.current;
+      const total = c.clientWidth;
+      const childs = c.childNodes;
+      const withoutLast = [].map.call(childs, c => c).slice(0, -1);
+      const widthOfLast = total - withoutLast.reduce((width, el) => {
+        const style = window.getComputedStyle(el);
+        const marginLeft = parseInt(style.marginLeft, 10);
+        const marginRight = parseInt(style.marginRight, 10);
+        return width + el.clientWidth + marginLeft + marginRight;
+      }, 0);
+      this.setState({widthOfLast});
+    }
+
+    catch(e) { console.warn('cannot calculate width of breadcrumbs:', e) }
   }
 
   onClick(e, bc) {
@@ -41,7 +67,7 @@ class Breadcrumbs extends Component {
         <BreadcrumbRightArrow />
       </BreadcrumbDivider>
     ) : last ? (
-      <LastBreadcrumb theme={this.props.theme} key={i}>
+      <LastBreadcrumb theme={this.props.theme} key={i} width={this.state.widthOfLast} wrap={this.props.wrap}>
         {breadcrumb.title}
       </LastBreadcrumb>
     ) : (
@@ -71,7 +97,7 @@ class Breadcrumbs extends Component {
     const filled = this.getFilledWithDividers(this.props.breadcrumbs);
 
     return (
-      <BreadcrumbsWrap {...otherProps}>
+      <BreadcrumbsWrap {...otherProps} innerRef={this.container}>
         {filled.map(this.renderBreadcrumb)}
       </BreadcrumbsWrap>
     );
@@ -82,6 +108,7 @@ Breadcrumbs.propTypes = {
   breadcrumbs: PropTypes.arrayOf(PropTypes.object),
   withCustomLinks: PropTypes.bool,
   theme: PropTypes.object,
+  wrap: PropTypes.bool,
 };
 
 Breadcrumbs.defaultProps = {
